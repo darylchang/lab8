8812789b30dfca4b378e96d33226120a3ae3a858
vagrant <vagrant@precise32.(none)>
2014-02-28 21:08:42 +0000

    analytics lab

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,18 @@ var projects = require('../projects.json');
  * GET home page.
  */
 
-exports.view = function(req, res){
-  	res.render('index', projects);
+exports.view = function(req, res) {
+	var grid = false;
+  	res.render('index', {
+  		'projects': projects,
+  		'grid': grid
+  	});
 };
+
+exports.viewGrid = function(req, res) {
+	var grid = true;
+	res.render('index', {
+		'projects': projects,
+		'grid': grid
+	});
+}
